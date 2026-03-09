12e0be5f1ab436b588434d1412b6adf1cd4d2dc3
mc <Mario_cane.do@hotmail.com>
2026-03-09 15:33:07 -0300

    proyecto De transacciones DOllar terminado

--- app/src/main/java/com/example/dollar_room_pm/View/FirstFragment.kt
+++ app/src/main/java/com/example/dollar_room_pm/View/FirstFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.dollar_room_pm.Model.Local.AppDatabase
 import com.example.dollar_room_pm.Model.TransactionRepository
 import com.example.dollar_room_pm.R
 import com.example.dollar_room_pm.ViewModel.TransactionViewModel
@@ -49,8 +52,58 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
          // hacer las conexiones para insertar datos
+
+        repository = TransactionRepository(AppDatabase.getDataBase(requireContext()).dollarTransactionDao())
         // mostrar en el recycler view
+
+        binding.transactionRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.transactionRecyclerView.adapter = adapter
+
+
+        // observamos los cambios en liveData  desde transacciones
+
+        viewModel.transaction.observe(viewLifecycleOwner){
+
+
+            list ->
+
+            adapter.submitList(list)
+        }
+        viewModel.loadTransactions()
+
+
+            // btn comprar
+
+                 binding.buyButton.setOnClickListener { handleTransacction("BUY") }
+           // btn vender
+
+               binding.buyButton.setOnClickListener { handleTransacction("SELL") }
+
     }
+
+
+
+    private fun  handleTransacction( type : String){
+
+        val amountText = binding.amountEditText.text.toString()
+
+        if(amountText.isEmpty()){
+
+            Toast.makeText(requireContext(), "Ingrese un monto", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val amount = amountText.toDouble()
+        // guardar en la bd
+        viewModel.addTransaction(type, amount)//agrega al repositorio y actualiza LiveData
+        binding.amountEditText.text.clear()
+        Toast.makeText(requireContext(), "$type $amount USD registrado", Toast.LENGTH_SHORT).show()
+
+    }
+
+
+
+
 
 
     // función para ingresar transacciones de Dollar
